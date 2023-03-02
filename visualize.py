8ef5465234fef45a507f694061d5bc27f5b28bc7
christian patience <christianpatience@gmail.com>
2023-03-02 13:57:02 -0500

    updated simulation visualize.py

--- visualize.py
+++ visualize.py
@@ -3,6 +3,9 @@ import pandas as pd
 import process_files, foobank
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib import gridspec
+
+INPUT_FILENAME = "./output/test_data2.csv"
 
 def listRangeInclusive(beg: int, end: int) -> list[int]:
     '''
@@ -28,9 +31,9 @@ def getDataDisplayed(data: np.ndarray, row: int, thickness: int) -> np.ndarray:
 
 # GRAPH VISUALS
 shapes = {
-    'CPOX_UPPER_COLS': rangeCols(1,5),
-    'CPOX_LOWER_COLS': rangeCols(6,10),
-    'FT_COLS': rangeCols(11,15),
+    'CPOX_UPPER_COLS': rangeCols(1,20),
+    'CPOX_LOWER_COLS': rangeCols(21,40),
+    'FT_COLS': rangeCols(41,60),
     'CPOX_LOWER_VOLUME': 10,
     'CPOX_UPPER_VOLUME': 30,
     'FT_VOLUME': 100
@@ -41,22 +44,25 @@ lowerThickness = baseThickness# * shapes['CPOX_LOWER_VOLUME']
 ftThickness = baseThickness# * shapes['FT_VOLUME']
 
 # GET DATA
-filename = "./output/test_data.csv"
-df = process_files.import_sim_data(filename)
+df = process_files.import_sim_data(INPUT_FILENAME)
 cols = foobank.get_section_names(df)
 dataAll = df.loc[:, cols].values
 dataCPOXUPPER = df.loc[:, shapes['CPOX_UPPER_COLS']].values
 dataCPOXLOWER = df.loc[:, shapes['CPOX_LOWER_COLS']].values
 dataFT = df.loc[:, shapes['FT_COLS']].values
 frames = dataAll.shape[0]
-fps = 30 # frames per second
+fps = 120 # frames per second
 spf = 0.01 # seconds per frame
 timeOffset = df.t.iloc[0]
 timeFinal = df.t.iloc[-1] - timeOffset
 
 # FIGURE
-
-fig = plt.figure(figsize=(8,6))
+gridspec_kw = {
+    'width_ratios': [1,1,4],
+    'height_ratios': [2]
+}
+fig = plt.figure()
+spec = gridspec.GridSpec(ncols=3, nrows=2, width_ratios=[1,1,2], height_ratios=[1,3])
 
 # plt.subplot(1,3,1)
 # im = plt.imshow(
@@ -67,7 +73,7 @@ fig = plt.figure(figsize=(8,6))
 #     vmax = 1
 # )
 
-plt.subplot(1,3,1)
+ax1 = fig.add_subplot(spec[0,0])
 imCPOXUPPER = plt.imshow(
     X = getDataDisplayed(data=dataCPOXUPPER, row=0, thickness=upperThickness),
     interpolation='none',
@@ -76,7 +82,7 @@ imCPOXUPPER = plt.imshow(
     vmax = 1
 )
 
-plt.subplot(1,3,2)
+ax2 = fig.add_subplot(spec[0,1])
 imCPOXLOWER = plt.imshow(
     X = getDataDisplayed(data=dataCPOXLOWER, row=0, thickness=lowerThickness),
     interpolation='none',
@@ -85,7 +91,7 @@ imCPOXLOWER = plt.imshow(
     vmax = 1
 )
 
-plt.subplot(1,3,3)
+ax3 = fig.add_subplot(spec[:,2])
 imFT = plt.imshow(
     X = getDataDisplayed(data=dataFT, row=0, thickness=ftThickness),
     interpolation='none',
@@ -101,14 +107,21 @@ def init():
     return [imCPOXUPPER, imCPOXLOWER, imFT]
 
 def animate(frame):
-    plt.title(f"{df.t.iloc[frame]-timeOffset:.2f}/{timeFinal:.2f}")
+    print(f"{frame/frames*100:.0f}%")
+    plt.title(f"{(df.t.iloc[frame]-timeOffset):.1f}/{timeFinal:.1f}") # 
     
     imCPOXUPPER.set_data(getDataDisplayed(data=dataCPOXUPPER, row=frame, thickness=upperThickness))
     imCPOXLOWER.set_data(getDataDisplayed(data=dataCPOXLOWER, row=frame, thickness=lowerThickness))
     imFT.set_data(getDataDisplayed(data=dataFT, row=frame, thickness=ftThickness))
     return [imCPOXUPPER, imCPOXLOWER, imFT]
 
+def save_animation(filename: str, fps: int, save: bool):
+    if (save): 
+        print("Saving", end="...")
+        anim.save(filename, fps=fps, extra_args=['-vcodec', 'libx264'])
+        print("Complete")
+
 anim = FuncAnimation(fig, animate, init_func=init, frames=frames, interval=1, blit=True, repeat=False)
 
 # plt.show()
-anim.save('./animations/basic_animation.mp4', fps=fps, extra_args=['-vcodec', 'libx264'])
+save_animation(filename='./animations/basic_animation.mp4', fps=fps, save=True)
